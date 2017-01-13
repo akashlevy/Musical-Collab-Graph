002bd235797c60ba887bb7c67036471d4f52a8fd
vbpo <vbpo@princeton.edu>
2017-01-13 15:56:49 -0500

    Beethoven tree analysis

--- metrics.py
+++ metrics.py
@@ -7,23 +7,41 @@ from Queue import Queue
 import time
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 G = nx.read_gpickle('graph.pickle')
 
-#print(G.nodes())
+print("Number of nodes in graph:")
+print(nx.number_of_nodes(G))
 
-nx.bfs_tree(G, 'spotify:artist:3TVXtAsR1Inumwj472S9r4')
+shortestPathLengths = nx.shortest_path_length(G, source ='spotify:artist:3TVXtAsR1Inumwj472S9r4')
 
-nx.write_gpickle(G, 'draketree.pickle')
+distances = shortestPathLengths.values()
 
-try:
-    from networkx import graphviz_layout
-except ImportError:
-    raise ImportError("This example needs Graphviz and either PyGraphviz or Pydot")
+histogram = [0] * 10
+# Calculate distances
+for i in range(len(distances)):
+	histogram[distances[i]] += 1
 
-pos=nx.graphviz_layout(G,labels=dict((n, d['name'].replace('$$', 'ss')) for n, d in G.nodes(data=True)))
-plt.figure(figsize=(8,8))
-nx.draw(G,pos,labels=dict((n, d['name'].replace('$$', 'ss')) for n, d in G.nodes(data=True)),node_size=20,alpha=0.5,node_color="blue")
-plt.axis('equal')
-#plt.savefig('circular_tree.png')
-plt.show()
+print histogram
+
+print(len(shortestPathLengths))
+
+
+G = nx.read_gpickle('beethoven.pickle')
+
+# Create tree of nodes to display in html
+beethovenTree = nx.bfs_tree(G, source = 'spotify:artist:2wOqMjp9TyABvtHdOSOTUS')
+
+# Output as a pickle file
+nx.write_gpickle(G, 'beethovenTree.pickle')
+
+
+
+
+
+
+
+
+
+
